b2a09c82c657811a0227c6258c9f2cf7916390df
heesun <heesun9793@naver.com>
2023-03-21 17:23:27 +0900

    최종 수정

--- src/main/java/shop/mtcoding/miniproject2/model/CompanyScrapRepository.java
+++ src/main/java/shop/mtcoding/miniproject2/model/CompanyScrapRepository.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import shop.mtcoding.miniproject2.dto.companyScrap.CompanyScrapOutDto;
-import shop.mtcoding.miniproject2.dto.companyScrap.CompanyScrapResDto.CompanyScrapWithResumeInfoResDto;
 
 @Mapper
 public interface CompanyScrapRepository {
@@ -15,10 +14,6 @@ public interface CompanyScrapRepository {
         public List<CompanyScrapOutDto> findByIdResumeAndSkillFilter(int id);
 
         public CompanyScrap findById(int id);
-
-        public CompanyScrap findByCInfoIdAndResumeId(@Param("cInfoId") int cInfoId, @Param("resumeId") int resumeId);
-
-        public List<CompanyScrapWithResumeInfoResDto> findresumeTitleAndNameByCInfoId(int cInfoId);
 
         public int insert(@Param("cInfoId") int cInfoId,
                         @Param("resumeId") int resumeId);
