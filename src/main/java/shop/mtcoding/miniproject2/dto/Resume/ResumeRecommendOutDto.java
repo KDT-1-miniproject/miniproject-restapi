4d13d7ddfb5fcaa0c3a9e28d6d3082972ac8e20e
heesun <heesun9793@naver.com>
2023-03-22 09:33:27 +0900

    test

--- src/main/java/shop/mtcoding/miniproject2/dto/Resume/ResumeRecommendOutDto.java
+++ src/main/java/shop/mtcoding/miniproject2/dto/Resume/ResumeRecommendOutDto.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import shop.mtcoding.miniproject2.dto.Resume.ResumeRecommendOutDto.ResumeRecommendArrDto;
 
 public class ResumeRecommendOutDto {
 
