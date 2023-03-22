1a6a22e56327255c35dd4a33caf7f11d29def450
heesun <heesun9793@naver.com>
2023-03-22 10:53:07 +0900

    기업 인재 추천 수정 ㅠㅠ

--- src/main/java/shop/mtcoding/miniproject2/dto/Resume/ResumeRes.java
+++ src/main/java/shop/mtcoding/miniproject2/dto/Resume/ResumeRes.java
@@ -2,8 +2,12 @@ package shop.mtcoding.miniproject2.dto.Resume;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import shop.mtcoding.miniproject2.dto.Resume.ResumeRes.ResumeRecommendDto.PersonDto;
+import shop.mtcoding.miniproject2.dto.Resume.ResumeRes.ResumeRecommendDto.SkillDto;
 
 public class ResumeRes {
 
@@ -11,27 +15,52 @@ public class ResumeRes {
     @Setter
     public static class ResumeRecommendDto {
         private Integer id;
-        private String name;
         private String title;
-        private String skills;
+        private PersonDto person;
+        private SkillDto skill;
+
+        @Getter
+        @Setter
+        public static class PersonDto {
+            private Integer id;
+            private String name;
+        }
+
+        @Getter
+        @Setter
+        public static class SkillDto {
+            private Integer id;
+            private String skills;
+        }
     }
 
+    @NoArgsConstructor
     @Getter
     @Setter
-    public static class ResumeRecommendArrDto {
+    public static class ResumeRecommendScrapDto {
         private Integer id;
-        private String name;
         private String title;
-        private String[] skills;
+        private PersonDto person;
+        private SkillDto skill;
         private Integer scrap;
+
+        public ResumeRecommendScrapDto(ResumeRecommendDto rDto) {
+            this.id = rDto.getId();
+            this.title = rDto.getTitle();
+            this.person = rDto.getPerson();
+            this.skill = rDto.getSkill();
+        }
+
     }
 
+    @NoArgsConstructor
+    @AllArgsConstructor
     @Getter
     @Setter
     public static class ResumeWithPostInfoRecommendDto {
         private Integer postId;
         private String title;
-        private List<ResumeRecommendArrDto> resumes;
+        private List<ResumeRecommendScrapDto> resumes;
     }
 
 }
